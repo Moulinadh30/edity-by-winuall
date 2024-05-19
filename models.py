7bc9ddc125e218225ecea2eb8eec4a3265233faf
Moulinadh <115913000+Moulinadh30@users.noreply.github.com>
2024-05-19 21:21:14 +0530

    Create models.py

---
+++ models.py
@@ -0,0 +1,10 @@
+class Book:
+    def __init__(self, id, title, author):
+        self.id = id
+        self.title = title
+        self.author = author
+
+books = [
+    Book(1, 'Book 1', 'Author 1'),
+    Book(2, 'Book 2', 'Author 2')
+]
